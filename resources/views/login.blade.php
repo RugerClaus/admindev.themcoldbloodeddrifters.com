11bacf6e43066efe299b6761f7ae5b9aeb936bac
rugerclaus <rogerrock12games@gmail.com>
2025-08-17 17:02:49 -0600

    updated title for login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Coldblooded Admin Login</title>
 </head>
 <body>
     <section id="login">
