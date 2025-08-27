d30ac4febd2ccb82cca24e8bcd8547a7c0926260
rugerclaus <rogerrock12games@gmail.com>
2025-08-26 23:34:04 -0600

    fixed css and removed logout button in editor views

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,7 @@ function showPage(target) {
     document.querySelectorAll('.page').forEach(page => page.classList.add('hidden'));
     document.getElementById(target).classList.remove('hidden')
     document.querySelector('.menu').classList.add('hidden')
+    document.querySelector('.logout_form').classList.add('hidden')
 }
 
 document.querySelectorAll('.card').forEach(card => {
@@ -18,6 +19,7 @@ document.querySelectorAll('.close_section_button').forEach(button => {
     button.addEventListener('click', () => {
         document.querySelectorAll('.page').forEach(page => page.classList.add('hidden'))
         document.querySelector('.menu').classList.remove('hidden')
+        document.querySelector('.logout_form').classList.remove('hidden')
         window.location.hash = ''
     })
 })
