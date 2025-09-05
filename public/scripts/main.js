0a37fbf04a08d8f86cd15f53ad75e444e15493c1
rugerclaus <rogerrock12games@gmail.com>
2025-09-05 13:26:10 -0600

    testing back page on logout form number whatever

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,12 @@
 const body = document.querySelector('body')
 
+const logout_form = document.querySelector('.logout_form')
+
+if (window.location.hash == '')
+{
+    logout_form.classList.remove('hidden')
+}
+
 function showPage(target) {
     document.querySelectorAll('.page').forEach(page => page.classList.add('hidden'));
     document.getElementById(target).classList.remove('hidden')
